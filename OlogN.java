0e97857a846d97a4054dcc0bc4067b1726bb3184
Ailton josé santos silva júnior <151879636+ailtonXL@users.noreply.github.com>
2025-11-24 09:21:28 -0300

    Create OlogN.java with example of array printing

---
+++ OlogN.java
@@ -0,0 +1,9 @@
+public class ON_Exemplo {
+    public static void main(String[] args) {
+        int[] numeros = {1, 2, 3, 4, 5};
+
+        for (int n : numeros) {
+            System.out.println(n);
+        }
+    }
+}
