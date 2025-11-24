04a1dd52519f756b01105d02b24573e9620b533f
Ailton josé santos silva júnior <151879636+ailtonXL@users.noreply.github.com>
2025-11-24 09:23:03 -0300

    Implement binary search in OlogN.java

--- OlogN.java
+++ OlogN.java
@@ -1,9 +1,26 @@
-public class ON_Exemplo {
-    public static void main(String[] args) {
-        int[] numeros = {1, 2, 3, 4, 5};
+import java.util.Arrays;
 
-        for (int n : numeros) {
-            System.out.println(n);
+public class OlogN_Exemplo {
+    public static void main(String[] args) {
+        int[] numeros = {2, 4, 6, 8, 10, 12, 14, 16};
+
+        int alvo = 10;
+        int inicio = 0;
+        int fim = numeros.length - 1;
+
+        while (inicio <= fim) {
+            int meio = (inicio + fim) / 2;
+
+            if (numeros[meio] == alvo) {
+                System.out.println("Encontrado: " + numeros[meio]);
+                break;
+            } 
+            else if (numeros[meio] < alvo) {
+                inicio = meio + 1;
+            } 
+            else {
+                fim = meio - 1;
+            }
         }
     }
 }
